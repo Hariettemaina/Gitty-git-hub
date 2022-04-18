bf5d640d30ce5882a4a41dcf156117e30fdb44a2
Hariettemaina <hariette.maina@student.moringaschool.com>
2022-04-18 13:44:02 +0300

    added paths

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { GithubUserDetailsComponent } from './github-user-details/github-user-details.component';
+import { GithubuserRepoDetailsComponent } from './githubuser-repo-details/githubuser-repo-details.component';
+import { HomepageComponent } from './homepage/homepage.component';
 
 const routes: Routes = [
-  { path: '', pathMatch: 'full', redirectTo: 'home' }
+  { path: '', pathMatch: 'full', redirectTo: 'home' },
+  { path: 'home', component: HomepageComponent },
+  { path: 'searchUser/:login', component: GithubUserDetailsComponent },
+  { path: 'searchUser', component: GithubUserDetailsComponent },
+  { path: 'searchUserRepositories', component: GithubuserRepoDetailsComponent },
 ];
 
 @NgModule({

e37c5fc8f8a726b110b9a8da128c1314e408daf7
Hariettemaina <hariette.maina@student.moringaschool.com>
2022-04-18 10:54:55 +0300

    imported users repos and environment

--- src/app/github.service.ts
+++ src/app/github.service.ts
@@ -1,5 +1,12 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import  'rxjs/add/operator/map';
+import { Users } from './users';
+import { Repository } from './repository';
+import { environment } from 'src/environments/environment';
+
+
+
 @Injectable({
   providedIn: 'root'
 })

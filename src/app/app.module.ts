9c8043150b2b79161a52b9e9d8d407b0651a13b1
Hariettemaina <hariette.maina@student.moringaschool.com>
2022-04-18 12:55:29 +0300

    generated hover directive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HighlightHoverDirective } from './highlight-hover.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
+    HighlightHoverDirective,
     ],
   imports: [
     BrowserModule,

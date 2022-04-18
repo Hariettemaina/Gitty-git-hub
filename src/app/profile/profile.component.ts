9c114f2dfa63453a835ff28d2a6064ef695acbd3
Hariettemaina <hariette.maina@student.moringaschool.com>
2022-04-18 11:46:03 +0300

    added funtion

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,12 +1,17 @@
 import { Component, OnInit } from '@angular/core';
-
+import {GithubService} from '../github.service';
+import 'rxjs/add/operator/map';
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent implements OnInit {
+ users!: any[];
+ repository!: any[];
+ username!: string;
 
+ 
   constructor() { }
 
   ngOnInit(): void {

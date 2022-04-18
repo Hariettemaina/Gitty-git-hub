bfed0faf2f10df29770c49771f4ba19bf211fa91
Hariettemaina <hariette.maina@student.moringaschool.com>
2022-04-18 17:52:40 +0300

    changed gsome files

--- src/app/github-user-details/github-user-details.component.ts
+++ src/app/github-user-details/github-user-details.component.ts
@@ -9,10 +9,60 @@ import { Repository } from '../repository';
   styleUrls: ['./github-user-details.component.css']
 })
 export class GithubUserDetailsComponent implements OnInit {
+  userName: string = '';
+  isError: boolean = false;
+  userNameLink: any;
+  userData: Users;
+  showUserDetails: boolean = false;
+  repoData: Repository[];
 
-  constructor() { }
 
-  ngOnInit(): void {
+  isLoading: boolean = true;
+  constructor(
+    private router: ActivatedRoute,
+    private dataService: GithubDataService
+  ) {}
+  getUserData() {
+    if (this.userName === '') {
+      this.isLoading = false;
+      this.isError = true;
+      return;
+    }
+    this.showUserDetails = true;
+    this.isLoading = true;
+    this.dataService.getUserData(this.userName).then((data) => {
+      this.isLoading = false;
+      this.isError = false;
+      this.userData = data;
+    });
+
+    this.dataService.getUserRepoData(this.userName).then((data) => {
+      this.repoData = data;
+    });
+    this.userName = '';
   }
 
+  ngOnInit() {
+    this.isLoading = true;
+    this.userName = 'Hariettemaina';
+    this.userNameLink = this.router.snapshot.paramMap.get('login');
+
+    this.dataService.getUserData(this.userNameLink).then((data) => {
+      this.showUserDetails = true;
+
+      if (data.login === 'null') {
+        this.getUserData();
+        this.userName = '';
+        this.isLoading = false;
+        return;
+      }
+
+      this.userData = data;
+      this.dataService.getUserRepoData(this.userNameLink).then((data) => {
+        this.repoData = data;
+      });
+      this.isLoading = false;
+      this.userName = '';
+    });
+  }
 }

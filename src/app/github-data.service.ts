b64b6b3515f302c26bdc9978e86d2a9e1ec149df
Hariettemaina <hariette.maina@student.moringaschool.com>
2022-04-18 14:01:04 +0300

    imported http,users,envoronment and repo

--- src/app/github-data.service.ts
+++ src/app/github-data.service.ts
@@ -1,5 +1,8 @@
 import { Injectable } from '@angular/core';
-
+import { environment } from '../environments/environment'
+import { HttpClient } from '@angular/common/http';
+import { Users } from './users'
+import { Repository } from './repository';
 @Injectable({
   providedIn: 'root'
 })

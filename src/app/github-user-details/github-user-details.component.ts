f5505345063bf8d1a611e6376a96e5b89d4524bf
Hariettemaina <hariette.maina@student.moringaschool.com>
2022-04-18 17:33:35 +0300

    imprted

--- src/app/github-user-details/github-user-details.component.ts
+++ src/app/github-user-details/github-user-details.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Users } from '../users';
+import { ActivatedRoute } from '@angular/router';
+import { GithubDataService } from '../github-data.service';
+import { Repository } from '../repository';
 @Component({
   selector: 'app-github-user-details',
   templateUrl: './github-user-details.component.html',
--- src/app/githubuser-repo-details/githubuser-repo-details.component.ts
+++ src/app/githubuser-repo-details/githubuser-repo-details.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import { GithubDataService } from '../github-data.service';
+import { Repository } from '../repository';
 @Component({
   selector: 'app-githubuser-repo-details',
   templateUrl: './githubuser-repo-details.component.html',
@@ -7,9 +8,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GithubuserRepoDetailsComponent implements OnInit {
 
-  constructor() { }
+  searchTerm: string = '';
+  isError: boolean = false;
+  isDataError: boolean = false;
+  repoData!: Repository[];
+  arrayData: any;
+  isLoading: boolean = false;
+  constructor(private dataService: GithubDataService) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  getUserData() {
+    this.isLoading = true;
+    this.isError = false;
+    this.isDataError = false;
+
+    if (this.searchTerm === '') {
+      this.isLoading = false;
+      this.isError = true;
+      return;
+    }
+
+    this.isLoading = true;
+
+    this.dataService.getRepoData(this.searchTerm).then((data) => {
+      this.isLoading = false;
+      this.arrayData = Object.entries(data);
+      let repositoryData = this.arrayData[2];
+
+      let convertRepositoryData =
+        repositoryData[Object.keys(repositoryData)[1]];
+
+      if (convertRepositoryData.length == 0) {
+        this.isDataError = true;
+        return;
+      }
+      this.repoData = convertRepositoryData;
+    });
+    this.searchTerm = '';
   }
-
 }
